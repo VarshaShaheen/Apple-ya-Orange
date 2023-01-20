4dca0067157338e627861ba6fe0136d5825a41ff
VarshaShaheen <varshashaheen2003@gmail.com>
2023-01-20 09:35:32 +0530

    documented

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ from PIL import Image
 class SimpleModel(nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
+        """Creating kernels for the convolutional layers"""
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        """Creating the fully connected layers"""
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 2)
